c76e31afc42e4ecbe287f6b65e9a79706230945e
Oscar Martinez <ojmartinezma@unal.edu.co>
2023-10-08 14:19:23 -0500

    Creación de los componentes de visualización de productos en la poagia principal

--- Front/anunciate/src/components/MainPage.vue
+++ Front/anunciate/src/components/MainPage.vue
@@ -3,16 +3,20 @@
   <div>
     <MainHeader></MainHeader>
   </div>
+  <div>
+    <ProductList></ProductList>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import NavBar from "./NavBar.vue";
 import MainHeader from "./MainHeader.vue";
+import ProductList from "./ProductList.vue";
 
 export default defineComponent({
   name: "MainPage",
-  components: { NavBar, MainHeader },
+  components: { NavBar, MainHeader, ProductList },
 });
 </script>
 
